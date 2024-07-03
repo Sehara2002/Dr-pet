3a585e0bf6978819b9d211888eb9c576f75752ed
Sehara2002 <fernandomsa.22@uom.lk>
2024-07-04 01:58:16 +0530

    Adding new Styles

--- src/components/Homebanner.jsx
+++ src/components/Homebanner.jsx
@@ -1,27 +1,50 @@
 import React from 'react'
 import "./SCSS/banner.scss"
 import backFoot from "../assets/Images/paw.png"
+import dog2 from "../assets/Images/dog2.png"
+import clinic from "../assets/Images/logo2.png"
 function Homebanner() {
     return (
-        <div className="banner-div">
-            <img src={backFoot} alt="" className="backfootImage" />
-            <div className="row">
-                <div className="col-sm-6">
-                    <div className="row">
-                        <div className="col-sm-6">
-                            <div>C</div>
-                        </div>
-                        <div className="col-sm-6">
-                            <div className="phrases"></div>
-                            <div className="phrases"></div>
-                            <div className="phrases"></div>
+        <div>
+            <div className="container-fluid banner-section">
+                <img src={backFoot} alt="" className="backround-image"/>
+                <div className="row">
+                    <div className="col-sm-6">
+                        <div className="container-fluid">
+                            <div className="row">
+                                <div className="col-sm-6">
+                                    <div className="text-boxes-main">
+                                        <div className="cap-text text-end">
+                                            C
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-sm-6">
+                                    <div className="text-boxes">
+                                        <div className="line-text">ONNECT</div>
+                                        <div className="line-text">ARE</div>
+                                        <div className="line-text">URE</div>
+                                        <div className="line-text italictext text-center">With</div>
+                                        <div className="banner-container-image">
+                                            <div className="row">
+                                                <div className="col-sm-6">
+                                                    <img src={clinic} alt="" className="clinic-image" />
+                                                </div>
+                                                <div className="col-sm-6">
+                                                    <p className='clinic-text text-start'>Friendly Clinic</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="col-sm-6">
+                    <div className="col-sm-6">
+                        <img src={dog2} alt="" className="banner-image"/>
 
+                    </div>
                 </div>
-
             </div>
         </div>
     )
