b874bc4e4388eb3bb7294c27f113db053f20e944
Sehara2002 <fernandomsa.22@uom.lk>
2024-07-05 17:09:44 +0530

    Removing Manifest Icons

--- src/components/Servicescomponent.jsx
+++ src/components/Servicescomponent.jsx
@@ -3,35 +3,37 @@ import React from 'react'
 import "bootstrap/dist/css/bootstrap.min.css";
 // Bootstrap Bundle JS
 import "bootstrap/dist/js/bootstrap.bundle.min";
+import "./SCSS/servicescomponent.scss";
 function Servicescomponent() {
-  return (
-    <div>
-        <div className="container-fluid">
-            <div className="row">
-                <div className="service-container">
-                    24 Hours Emergency Services
+    return (
+        <div>
+            <div className="container">
+                <div className="row">
+                    <div className="block-container">
+                        <div className="service-container">
+                            24 Hours Emergency Services
+                        </div>
+                        <div className="line-container"></div>
+                        <div className="service-container">
+                            Friendly Staff
+                        </div>
+                        <div className="line-container"></div>
+                        <div className="service-container">
+                            Well Experienced Doctor
+                        </div>
+                        <div className="line-container"></div>
+                        <div className="service-container">
+                            Online Channeling
+                        </div>
+                        <div className="line-container"></div>
+                        <div className="service-container">
+                            Dental Services
+                        </div>
+                    </div>
                 </div>
-                <div className="line-container"></div>
-                <div className="service-container">
-                    Friendly Staff
-                </div>
-                <div className="line-container"></div>
-                <div className="service-container">
-                    Well Experienced Doctor
-                </div>
-                <div className="line-container"></div>
-                <div className="service-container">
-                    Online Channeling
-                </div>
-                <div className="line-container"></div>
-                <div className="service-container">
-                    Dental Services
-                </div>
-                <div className="line-container"></div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Servicescomponent
--- src/pages/Home/Homepage.jsx
+++ src/pages/Home/Homepage.jsx
@@ -4,6 +4,7 @@ import "../../components/SCSS/style-common.scss"
 import Homebanner from '../../components/Homebanner'
 import Servicescomponent from '../../components/Servicescomponent'
 
+
 function Homepage() {
   return (
     <div className="homepage-maincontainer">
