3502806bffd2d6e7c866e7c7881c44123f3413c3
Shiny Fernando <119037970+RominaFdo@users.noreply.github.com>
2024-07-12 02:12:56 +0530

    servicesV2
    Made some improvements in the Services page.

--- src/components/Servicebanner.jsx
+++ src/components/Servicebanner.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import "./CSS/servicebanner.css"
+import sb1 from "../assets/Images/sb1.png"
+import sb3 from "../assets/Images/sb3.png"
+
 
 function Servicebanner() {
     return (
         <div className="container-fluid banner">
-            <h1 className="banner-head">Our Services</h1>
+            <div>
+                <h1 className="banner-head-01">Tailored Pet</h1>
+                <h1 className="banner-head-02">Services</h1>
+                <img src={sb1} className="banner-head-img-01"/>
+                <img src={sb3} className="banner-head-img-03"/>
+                <img src={sb1} className="banner-head-img-02"/>
+            </div>
             <p className="banner-desc">At Dr Pet, we believe in treating your pets with the same love and care as we do our own. Our approach is personal and compassionate, ensuring your pet receives the best possible care every step of the way. We understand the special bond you share with your furry family members, and our dedicated team is here to support that bond through every stage of their life.</p>
             <p className="banner-desc">From routine check-ups to specialized treatments, we work closely with you to create a tailored healthcare plan that suits your pet’s unique needs. Trust, transparency, and partnership are at the core of our philosophy. Experience the difference that personalized, compassionate veterinary care can make for you and your pet at Dr Pet. For more information or to schedule an appointment, contact us today!</p>
         </div>
